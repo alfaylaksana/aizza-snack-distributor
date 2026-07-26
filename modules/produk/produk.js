ab4417499a9894bd30a68e5ebb2af33517f9e681
alfaylaksana <alfaylaksana@gmail.com>
2026-07-26 20:58:03 +0700

    Update produk.js

--- modules/produk/produk.js
+++ modules/produk/produk.js
@@ -103,3 +103,46 @@ alert(
 
 
 window.onload=tampilProduk;
+
+function simpanProduk(){
+
+let data = {
+
+kode: document.getElementById("kode").value,
+
+nama: document.getElementById("nama").value,
+
+kategori: document.getElementById("kategori").value,
+
+berat: Number(document.getElementById("berat").value),
+
+modal: Number(document.getElementById("modal").value),
+
+hargaToko: Number(document.getElementById("hargaToko").value),
+
+hargaJual: Number(document.getElementById("hargaJual").value),
+
+catatan: document.getElementById("catatan").value
+
+};
+
+
+if(!data.kode || !data.nama){
+
+alert("Kode dan nama produk wajib diisi");
+
+return;
+
+}
+
+
+produk.push(data);
+
+
+tampilProduk();
+
+
+alert("Produk berhasil ditambahkan");
+
+
+}
