658ae127981b825bbdd72bc331336f44cc59ce44
alfaylaksana <alfaylaksana@gmail.com>
2026-07-29 20:48:37 +0700

    Update produk.js

--- modules/produk/produk.js
+++ modules/produk/produk.js
@@ -1,10 +1,9 @@
-import { db } from "../../firebase.js";
+// import { db } from "../../firebase.js";
 
-import {
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
+// import {
+//   collection,
+//   getDocs
+// } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 alert("TES AIZZAY");
 alert("produk.js berjalan");
