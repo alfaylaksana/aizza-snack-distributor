4f03e5c26f6c8f974688568221845079291fa342
alfaylaksana <alfaylaksana@gmail.com>
2026-08-02 17:44:55 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // AIZZAY Snack Distributor
-// Service Worker Versi 0.27
+// Service Worker Versi 0.28
 
-const CACHE_NAME = "aizzay-v27";
+const CACHE_NAME = "aizzay-v28";
 
 const FILES_TO_CACHE = [
   "./",
