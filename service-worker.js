75cca8904e7ac4343457148ab2455bdb4a1851aa
alfaylaksana <alfaylaksana@gmail.com>
2026-08-04 10:58:38 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // AIZZAY Snack Distributor
-// Service Worker Versi 0.44
+// Service Worker Versi 0.45
 
-const CACHE_NAME = "aizzay-v44";
+const CACHE_NAME = "aizzay-v45";
 
 const FILES_TO_CACHE = [
   "./",
